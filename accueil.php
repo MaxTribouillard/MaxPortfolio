02301763486e75ad1c3de9fbdf063e6d93d4318e
Max <57811248+MaxTribouillard@users.noreply.github.com>
2024-10-31 15:30:51 +0100

    Initial commit

---
+++ accueil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Max .T - PORTFOLIO</title>
+    <link href="css//base.css" rel="stylesheet" />
+</head>
+<body>
+    
+<?php
+
+require_once("yaml/yaml.php");
+$data = yaml_parse_file("yaml/accueil.yaml");
+
+echo "<h1> Bienvenue sur le portfolio de ".$data['prenom']."</h1>";
+echo "<p> Je me présente  ".$data['presentation']."</p>";
+echo "<p>"
+
+
+?>
+
+</body>
+</html>
+
