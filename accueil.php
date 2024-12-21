05654aa8f3fd1ce3e6caa349de269e32b98ea290
Max <57811248+MaxTribouillard@users.noreply.github.com>
2024-12-21 20:16:07 +0100

    update des competences

--- accueil.php
+++ accueil.php
@@ -1,3 +1,19 @@
+<?php
+// Charger la bibliothèque Symfony/Yaml
+require 'yaml/vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+
+// Charger les données YAML
+$skills = [];
+try {
+    $skills = Yaml::parseFile('competences.yaml')['dev'];
+} catch (Exception $e) {
+    echo "Erreur de lecture du fichier YAML : " . $e->getMessage();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,34 +30,51 @@
 
 </head>
 <body>
-    
-<img class='background' src='css/2615525.jpg'>
+
+<?php
+// Illustration première page
+require_once("yaml/yaml.php");
+$homeData = yaml_parse_file("accueil.yaml");
+echo "<img id='home' class='background' src='".$homeData['illustration']."'>"
+
+?>
 
 <img class='nameimg' src='css/MAX-TRIBOUILLARD.png'>
 <div class="navbar">
         <ul class="menu">
             <li><a href="#home">HOME</a></li>
-            <li><a href="#mainTitle">MAX</a></li>
-            <li><a href="#aboutus">SKILLS</a></li>
+            <li><a href="#max">MAX</a></li>
+            <li><a href="#skillsTitle">SKILLS</a></li>
             <li><a href="#blog">WORK</a></li>
             <li><a href="#contact">CONTACT</a></li>
         </ul>
     </div>
-<!-- <h1 id='mainTitle' class='mainTitle'>Hello &#128516;</h1> -->
+<h1 id='mainTitle' class='mainTitle'>Hey ! &#128516;</h1>
 <div class='mainContent'>
 <?php
 
-require_once("yaml/yaml.php");
-$homeData = yaml_parse_file("accueil.yaml");
-
-echo "<h2 class='max'> Salut, moi c'est ".$homeData['prenom']." - Apprenti développeur. &#128187;</h2>";
-echo "<h4>".$homeData['accroche']."<br><br> Je me présente  ".$homeData['presentation']."</h4>";
-echo "<p>"
-
-
+// Accroche et présentation
+echo "<h2 id='max' class='max'> Salut, moi c'est ".$homeData['prenom']." - Apprenti développeur. &#128187;</h2>";
+echo "<div class='presentation'><h3 id='presentation'>Je me présente ".$homeData['presentation']." <br> ".$homeData['accroche']."</h3></div>";
 
 ?>
 </div>
+<h1 id='skillsTitle' class='skillsTitle'>Skills</h1>
+
+<div class="skillbars">
+        <?php foreach ($skills as $skill): ?>
+            <div class="skillbar-container">
+                <div class="skillbar">
+                    <span class="skillbar-name"><?= htmlspecialchars($skill['name']) ?></span>
+                    <div class="skillbar-bar" style="--skill-level: <?= htmlspecialchars($skill['level']) ?>%;">
+                        <div class="skillbar-fill"></div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+
 </body>
 </html>
 
